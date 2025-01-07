a140100a3cf2e6b8934bdc63b4ad795779a908d8
Divyanshu <akkisingh8266@gmail.com>
2025-01-07 11:52:23 +0530

    first program

---
+++ FirstProgram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FirstProgram
+{
+    // program for "if else" conditional control flow statements
+    class FirstProgram
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int x = 2;
+            int y = 3;
+            if (x > y)
+            {
+                Console.WriteLine("x is greater than y");
+            }
+            else
+            {
+                Console.WriteLine("y is greater than x");
+            }
+        }
+    }
+}
