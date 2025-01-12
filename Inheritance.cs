acf1b02823241c232451b4d7312e60728c4faac9
Divyanshu <akkisingh8266@gmail.com>
2025-01-12 12:03:23 +0530

    oops concepts

---
+++ Inheritance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Inheritance
+{
+	// Base or Parent  class
+	class Vehicle
+	{
+		public string brand = "Ford";
+		public void honk()
+		{
+			Console.WriteLine("peep, peep");
+		}
+	}
+
+	// Derived or child class
+	class Car : Vehicle // Inheritance syntax for inheriting one class to another
+	{
+		public string ModelName = "Mustang";
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Car car = new Car();
+			car.honk(); // we can access honk method by object of car class because of inheritance 
+
+            Console.WriteLine(car.brand + " " + car.ModelName  + " Coming");
+		}
+	}
+}
---
+++ MyConstructor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyConstructor
+{
+    class Car
+    {
+        public string model;
+        public Car()
+        {
+            model = "Safari"; // setting initial value for model using constructor
+        }
+
+        static void Main(string[] args)
+        {
+            Car Tata = new Car();
+            Tata.model = "Altroz"; // setting manually if we want later
+            Console.WriteLine(Tata.model);
+        }
+    }
+
+    // Constructor Parameters example
+    //class Car
+    //{
+    //    public string model;
+
+    //    public Car (string modelName)
+    //    {
+    //        model = modelName;
+    //    }
+        
+    //    static void Main(string[] args)
+    //    {
+    //        Car myCar = new Car("Fortuner");
+    //        Console.WriteLine(myCar.model);
+    //    }
+    //}
+
+}
+
+
